460b9c6bb83a4eac71c0b628952c570af1f8422d
Sudhir <sudhirmunna007@gmail.com>
2018-04-19 19:44:33 -0400

    building  list with map

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAZhSBoZWsJMhXQOf_hTKQoXCcm-bjZArI';
 
-YTSearch( {key: API_KEY, term: 'pavan kalyan'}, (data) => console.log(data) );
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+        this.state = { videos: [] };
+        YTSearch({ key: API_KEY, term: 'pavan kalyan' }, (data) => {
+            this.setState({ videos: data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
